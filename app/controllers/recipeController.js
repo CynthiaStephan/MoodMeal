04de5808d46c045570c0ab4b8a800085eb54db9d
CynthiaStephan <cynthia.stephan@institusolacroup.com>
2025-01-19 20:53:52 +0100

    Recipes routes ok

--- app/controllers/recipeController.js
+++ app/controllers/recipeController.js
@@ -1,9 +1,9 @@
 const { default: mongoose } = require('mongoose');
 const recipeModel = require('../models/Recipes');
+const moodModel = require('../models/Moods');
 
 class recipeController {
     constructor(recipeModel){
-
         this.recipeModel = recipeModel;
     }
 
@@ -23,6 +23,18 @@ class recipeController {
 
         } catch (err) {
             res.status(500).json({ error: err.message })
+        }
+    }
+
+    async getTagsList(req, res){
+        try{
+            const tags = await recipeModel.distinct('tags')
+            if(!tags){
+                return res.status(400).json({ error: "Tags non trouvées" })
+            }
+            res.status(200).json(tags);
+        } catch(err) {
+            res.status(400).json({ error : err.message });
         }
     }
 
@@ -49,6 +61,65 @@ class recipeController {
                 res.status(404).json({ error : 'Error while finding a recipe. Try again'});
             }
             res.status(200).json(recipe);
+
+        } catch(err) {
+            res.status(400).json({ error : err.message });
+        }
+    }
+
+    async getRecipeSuggestion(req, res){
+
+        const reqBody = req.body;
+        
+        try{
+            if(!reqBody) {
+                return res.status(400).json({ error: "Please send a ingredient, mood or tag"})
+            }
+
+            else if(reqBody.hasOwnProperty('ingredients')) {
+                const recipes = await recipeModel.find({ 
+                    ingredients: { $regex: reqBody.ingredients, $options: 'i' } 
+                });
+                if(!recipes){
+                    res.status(404).json({ error : 'Not found'});
+                }
+                res.status(200).json(recipes)
+
+            } else if(reqBody.hasOwnProperty('moods')) {
+                // getting tags form moods first
+                const getTagFromMood = await moodModel.find({ 
+                    mood: { $regex: reqBody.moods, $options: 'i' }
+                })
+                
+                if(getTagFromMood.lenght === 0){
+                    return res.status(400).json({ error: "Mood not valid"})
+                }
+                const tags = getTagFromMood[0].suggestedTags;
+                // then find recipes by tags
+                const recipes = await recipeModel.select({ 
+                    tags: tags
+                });
+
+                if(!recipes){
+                    res.status(404).json({ error : 'Not found' });
+                }
+
+                res.status(200).json(recipes)
+                
+            } else if(reqBody.hasOwnProperty('tags')) {
+                const recipes = await recipeModel.find({ 
+                    tags: { $regex: reqBody.tags, $options: 'i' } 
+                });
+                if(!recipes){
+                    res.status(404).json({ error : 'Not found'});
+                }
+                res.status(200).json(recipes)
+
+            } else {
+                return res.status(400).json({ error: "Erreur" })
+            }
+
+            res.status(200).json();
 
         } catch(err) {
             res.status(400).json({ error : err.message });
--- app/routes/recipeRoutes.js
+++ app/routes/recipeRoutes.js
@@ -6,6 +6,8 @@ const router = Router();
 
 router.get('/recipe/:id', recipeController.getRecipeById);
 router.get('/tag', recipeController.getRecipesByTag);
+router.get('/alltags', recipeController.getTagsList);
 router.get('/random', recipeController.getRandomRecipe);
+router.post('/suggestion', recipeController.getRecipeSuggestion);
 
 module.exports = router;
